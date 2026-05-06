d8e66a3c2f48615aa5700b01ce03ed706438ec6a
Piyush Verma <piyushverma079412@gmail.com>
2026-05-06 23:47:41 +0530

    Upload updated project to v10

---
+++ fraud-detector/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from detector.scam_detector import analyze_message
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+
+    if request.method == "POST":
+        message = request.form["message"]
+
+        score, reasons = analyze_message(message)
+
+        result = {
+            "score": score,
+            "reasons": reasons
+        }
+
+    return render_template("index.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
