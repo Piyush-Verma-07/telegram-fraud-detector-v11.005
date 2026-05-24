9c79b7fb2ba72a972bf6e2ac369355570b0f5764
Piyush Verma <piyushverma079412@gmail.com>
2026-05-24 11:40:13 +0530

    Production Tailwind optimization and PWA setup

---
+++ fraud-detector/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,jinja2}",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
